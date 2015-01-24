64f59d578de85315f60207cce3a7aa403b75b568
syaony <e125752@ie.u-ryukyu.ac.jp>
2015-01-24 18:46:32 +0900

    UnitMove移動処理更新

--- GGJ0124/Assets/_Script/UnitMove.cs
+++ GGJ0124/Assets/_Script/UnitMove.cs
@@ -6,11 +6,14 @@ public class NewBehaviourScript : MonoBehaviour {
 	private float gridLength;
 
 	private float moveTime;
+	private Vector2 localOrigin;
 
 	private float maxUpDis;
 	private float maxDownDis;
 	private float maxRightDis;
 	private float maxLeftDis;
+
+	private float dx,dy;
 
 	public enum MOVE_DIRECTION{
 		UP,
@@ -29,30 +32,47 @@ public class NewBehaviourScript : MonoBehaviour {
 		float moveX;
 		float moveY;
 
-		//moveX = 
-		//moveY = 
-
 		switch (currentDirection) {
 			case MOVE_DIRECTION.UP:
 				moveY = gridLength;
+				localOrigin.y += gridLength;
 				break;
 			case MOVE_DIRECTION.DOWN:
 				moveY = -gridLength;
+				localOrigin.y -= gridLength;
 				break;
 			case MOVE_DIRECTION.RIGHT:
 				moveX = gridLength;
+				localOrigin.x += gridLength;
 				break;
 			case MOVE_DIRECTION.LEFT:
-				moveX = -gridLength; 
+				moveX = -gridLength;
+				localOrigin -= gridLength;
 				break;
 		}
 
+		float minMoveX = -gridLength + (localOrigin - this.transform.position.x );
+		float maxMoveX = gridLength + (localOrigin - this.transform.position.x);
+		float minMoveY = -gridLength + (localOrigin - this.transform.position.y);
+		float maxMoveY = gridLength + (localOrigin - this.transform.position.y);
+
+		moveX += Random.Range(minMoveX, maxMoveX);
+		moveY += Random.Range (minMoveY, maxMoveY);
+
+		dx = -moveX / moveTime;
+		dy = -moveY / moveTime;
+
+		StartCoroutine("moveCorutine");
 
 	}
 
 	private IEnumerator moveCorutine(){
-		for (float time = 0.0f; time < moveTime;)
 
-		yield return null;
+		for (float time = 0.0f; time < moveTime;time += Time.deltaTime) {
+			this.transform.position.x += dx;
+			this.transform.position.y += dy;
+			yield return null;
+		} 
+
 	}
 }
